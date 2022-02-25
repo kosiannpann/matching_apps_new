c44993898b40efbf8c0a723784c6be1a83ca1d8d
kosiannpann <sunshainekh@gmail>
2022-02-26 08:20:34 +0900

    add product_list.html

--- plans/views.py
+++ plans/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from .models import Product
+
 # Create your views here.
 
 
 def index(request):
     return render(request, 'plans/index.html', {})
+
+
+def product_list(request, pk):
+    products = get_object_or_404(Product, pk=pk)
+    return render(request, 'plans/product_list.html', {'products': products})
