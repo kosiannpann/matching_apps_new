1f436919f5e0d28753c5249c813f748add719114
kosiannpann <sunshainekh@gmail>
2022-05-27 10:10:30 +0900

    ログアウトをbase.htmlに追加

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, resolve_url
 # from .forms import LoginForm, SignupForm
-from django.contrib.auth.views import LoginView
-from django.views import generic
+# from django.contrib.auth.views import LoginView
+# vfrom django.views import generic
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import DetailView
+from .models import CustomUser
 
 '''プロフィール画面'''
 
@@ -9,6 +12,24 @@ from django.views import generic
 def myprofile(request):
     return render(request, 'accounts/myprofile.html')
 
+
+'''いつかプロフィールを制限する機能を実装する'''
+# class OnlyYouMixin(UserPassesTestMixin):
+#    raise_exception = False     # set True if raise 403_Forbidden
+
+#    def test_func(self):
+#        user = self.request.user
+#        return user.pk == self.kwargs['pk'] or user.is_superuser
+
+
+# class MyprofileView(OnlyYouMixin, DetailView):
+#    model = CustomUser
+#    template_name = 'accounts/myprofile.html'
+
+#    def get_context_data(self, **kwargs):
+#        context = super().get_context_data(**kwargs)
+#        pass  # 必要に応じて処理を追加
+#        return context
 
 # '''ログイン'''
 
